e4e5671fec83e141ff89219c75433a0a7509338d
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-26 00:33:28 +0530

    add files

--- studentID.java
+++ studentID.java
@@ -7,9 +7,14 @@ public class studentID {
       String name = sc.nextLine();
       System.out.println("Enter your roll number:");
       int roll = sc.nextInt();
+      sc.nextLine(); // ye likhna pada kyunki nextInt ke baad nextLine ko call karna hota hai, otherwise nextLine skip ho jata hai
+      System.out.println("Enter your email:");
+      String email = sc.nextLine();
       System.out.println("Student name: " + name);
-        System.out.println("Student roll number: " + roll);
-        sc.close();   
+      System.out.println("Student roll number: " + roll);
+      System.out.println("Student email: " + email);
+      sc.close();
+      
     }
     
 }
---
+++ timepass.java
@@ -0,0 +1,15 @@
+public class timepass {
+    public static void main(String[] args){
+        int age = 18;
+        char grade = 'A';
+        boolean isStudent = true;
+        double height = 5.11;
+        String name = "Bhaskar Ranjan";
+        String car = "BMW";
+        System.out.println("My Name is " + name + ", I am " + age + " years old" + " And my favorite car is " + car);
+        System.out.println("My grade is " + grade);
+        System.out.println("Am I a student? " + isStudent);
+        System.out.println("My height is " + height + " feet.");
+
+    }
+}
