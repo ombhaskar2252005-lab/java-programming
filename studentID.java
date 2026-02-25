69b5ac5058256f914dd642ecf415676ee69c5007
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-25 23:12:18 +0530

    add files

---
+++ studentID.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class studentID {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your name:");
+      String name = sc.nextLine();
+      System.out.println("Enter your roll number:");
+      int roll = sc.nextInt();
+      System.out.println("Student name: " + name);
+        System.out.println("Student roll number: " + roll);
+        sc.close();   
+    }
+    
+}
