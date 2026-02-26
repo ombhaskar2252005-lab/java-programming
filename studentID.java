8edd6c546f43b271ac2501411316564eb35add24
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-26 14:48:18 +0530

    add files

--- studentID.java
+++ studentID.java
@@ -7,7 +7,7 @@ public class studentID {
       String name = sc.nextLine();
       System.out.println("Enter your roll number:");
       int roll = sc.nextInt();
-      sc.nextLine(); // ye likhna pada kyunki nextInt ke baad nextLine ko call karna hota hai, otherwise nextLine skip ho jata hai
+      sc.nextLine();
       System.out.println("Enter your email:");
       String email = sc.nextLine();
       System.out.println("Student name: " + name);
