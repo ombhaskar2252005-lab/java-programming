09c6170ee26048100e9171cb102872358ea13a4c
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-27 00:50:53 +0530

    add files

--- takingInput1.java
+++ takingInput1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class skibidi {
+public class takingInput1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name:");
