93b7bfd234200a31e73a6fab612310e1f1ea3933
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-26 14:35:59 +0530

    add files

---
+++ forEachLoop.java
@@ -0,0 +1,13 @@
+public class forEachLoop {
+    public static void main(){
+        int arr[] = {1, 2, 3, 4, 5 };
+        for (int om : arr){
+            System.out.println(om);
+        }
+
+        double arr1[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+        for (double om1 : arr1){
+            System.out.println(om1);
+        }
+    }
+}
