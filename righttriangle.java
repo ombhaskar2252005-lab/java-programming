92fa0ce135ed9a8272be001d5845dd92fb16b5e8
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-02-26 15:26:07 +0530

    add files

---
+++ righttriangle.java
@@ -0,0 +1,11 @@
+public class righttriangle {
+    public static void main() {
+        for(int r = 1; r<= 4; r++){
+            for(int c = 1; c <= r; c++){
+                System.out.print("*");
+            }
+            System.out.println(" "); // Move to the next line after each row
+        }
+    }
+    
+}
