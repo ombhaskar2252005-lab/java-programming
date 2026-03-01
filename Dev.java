e4c6aa3eb4970d6c74ff74170fa650d5ce0b6f80
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-03-02 00:02:13 +0530

    add files

--- Dev.java
+++ Dev.java
@@ -1,5 +1,10 @@
-class Dev {
+public class Dev {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        double x = 3.21;
+        double y = -4.56;
+        double max = Math.max(x, y);
+        System.out.println("maximum number is " + max);
+
     }
 }
