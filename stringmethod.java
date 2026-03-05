daacca27f721d581892f0a0534b18de025c117a5
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-03-05 23:56:20 +0530

    add files

---
+++ stringmethod.java
@@ -0,0 +1,9 @@
+public class stringmethod {
+    public static void main(String[] args) {
+        String name = "Bhaskar";
+        //int result = name.lastIndexOf('a');
+       // String result = name.toLowerCase();
+            String result = name.replace('a', 'A');
+        System.out.println(result);
+    }
+}
