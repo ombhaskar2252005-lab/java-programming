0687c9007719939a2f89cd96a0447490075fd8bb
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-05-09 09:25:19 +0530

    add files

---
+++ AccessModifiers.java
@@ -0,0 +1,24 @@
+class Parent{
+    private int a = 5;
+    public int b = 10;
+    protected int c = 15;
+    int d = 8; //default access modifier
+
+}
+class Child extends Parent{
+    void display(){
+    // System.out.println(a);
+    //System.out.println(b);
+   System.out.println(c);
+   // System.out.println(d);
+    }
+}
+
+
+public class AccessModifiers{
+    public static void main(String[] args){
+        System.out.println("access modifiers");
+        Child c = new Child();
+        c.display();
+    }
+}
