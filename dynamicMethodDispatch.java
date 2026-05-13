f85855ddd9afaf920e11f166ca1f8e3548119b3a
bhaskar ranjan <ombhaskar2252005@gmail.com>
2026-05-13 10:10:11 +0530

    add files

--- dynamicMethodDispatch.java
+++ dynamicMethodDispatch.java
@@ -15,6 +15,9 @@ class Cat extends Animal{
     void sound(){
         System.out.println("cat meows");
     }
+    void sleep(){
+        System.out.println("cat sleeps");
+    }
     
 }
 public class dynamicMethodDispatch {
@@ -22,11 +25,15 @@ public class dynamicMethodDispatch {
         Animal a = new Dog(); //upcasting
         //we have to convert a to dog(child)class to call eat method
         Dog d = (Dog) a; //downcasting
+        if (d instanceof Dog) {
+            System.out.println("d is an instance of Dog");
+        }
         a.sound(); 
         d.eat(); //limitation of upcasting - cannot call methods that are specific to the subclass
 
         Animal b = new Cat();
         b.sound(); //methods usi ka chalega jiska object hoga
+        Cat c = (Cat) b; //it is downcasting, we are converting parent class reference to child class reference
 
     }
 }
